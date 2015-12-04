e7d3c71121ace642639166eccbfd1f6eb2f102c4
Ian deBoisblanc <iandeboisblanc@gmail.com>
2015-12-04 11:22:26 -0800

    Reformatted twittler.js (no content change).

--- twittler.js
+++ twittler.js
@@ -1,45 +1,48 @@
-        var page = 'home';
-        $(document).ready(function(){
-          
 
-          refreshTweets();
-          $('#home').on('click', function() {
-            page = 'home';
-            refreshTweets();
-          });
-          $('button').on('click', refreshTweets);
-          $(document).on('click', '.user', function() {
-            page = $(this).text().slice(1);
-            refreshTweets();
-          });
+var page = 'home';
 
-        });
+$(document).ready(function(){
 
-        //Functions:
-        function createTweetHTML(tweet){
-          var $tweet = $('<div class="tweet"></div>');
-          $tweet.text(': ' + tweet.message);
-          // Create timeStamp element:
-          var timeStamp = tweet.created_at;
-          var $timeStamp = $('<footer class="timeStamp"></footer>');
-          $timeStamp.text(timeStamp);
-          $timeStamp.appendTo($tweet);
-          var $user = $('<a href="#" class="user">@' + tweet.user + '</a>');
-          $user.prependTo($tweet);
-          return $tweet;
-        }
+  refreshTweets();
 
-        function refreshTweets(){
-          var tweetStream;
-          $('main').text('');          
-          if(page === 'home') {
-            tweetStream = streams.home;
-            }
-          else {
-            tweetStream = streams.users[page];
-          }
-          for(var index = 0; index < tweetStream.length; index++) {
-            var $tweet = createTweetHTML(tweetStream[index]);
-            $tweet.prependTo($('main'));  
-          }
-        }
+  $('button').on('click', refreshTweets);
+  $('#home').on('click', function() {
+    page = 'home';
+    refreshTweets();
+  });
+  $(document).on('click', '.user', function() {
+    page = $(this).text().slice(1);
+    refreshTweets();
+  });
+
+});
+
+//Functions:
+function createTweetHTML(tweet){
+  var $tweet = $('<div class="tweet"></div>');
+  $tweet.text(': ' + tweet.message);
+  // Create timeStamp element:
+  var timeStamp = tweet.created_at;
+  var $timeStamp = $('<footer class="timeStamp"></footer>');
+  $timeStamp.text(timeStamp);
+  $timeStamp.appendTo($tweet);
+  var $user = $('<a href="#" class="user">@' + tweet.user + '</a>');
+  $user.prependTo($tweet);
+  return $tweet;
+}
+
+function refreshTweets(){
+  var tweetStream;
+  $('main').text('');          
+  if(page === 'home') {
+    tweetStream = streams.home;
+    }
+  else {
+    tweetStream = streams.users[page];
+  }
+  for(var index = 0; index < tweetStream.length; index++) {
+    var $tweet = createTweetHTML(tweetStream[index]);
+    $tweet.prependTo($('main'));  
+  }
+}
+
